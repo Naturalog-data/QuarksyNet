9e6775c1adb2b13bfa6cc9766f26f8901dc78ba7
Naturalog-data <99574832+Naturalog-data@users.noreply.github.com>
2022-04-01 10:19:37 -0700

    Create InitCluster.sh
    Inits Cluster

---
+++ initcluster.sh
@@ -0,0 +1,5 @@
+mkdir ~/.termux/boot
+cd ~/.termux/boot
+wget https://github.com/Naturalog-data/QuarksyNet/blob/main/startssh
+cd ~/
+wget https://raw.githubusercontent.com/Naturalog-data/QuarksyNet/main/DLNetHunter

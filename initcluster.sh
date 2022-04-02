feaf1d9a70a977ab8a7eda7b6080ad18d6841fa0
Naturalog-data <99574832+Naturalog-data@users.noreply.github.com>
2022-04-02 07:14:30 -1100

    Delete initcluster.sh
    Cleaning

--- initcluster.sh
+++
@@ -1,5 +0,0 @@
-mkdir ~/.termux/boot
-cd ~/.termux/boot
-wget https://raw.githubusercontent.com/Naturalog-data/QuarksyNet/main/startssh
-cd ~/
-wget https://raw.githubusercontent.com/Naturalog-data/QuarksyNet/main/DLNetHunter

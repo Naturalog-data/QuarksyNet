9295d2e456d068498d648eda433bd0b7887c4b16
Naturalog-data <99574832+Naturalog-data@users.noreply.github.com>
2022-04-02 15:27:30 -0700

    Update dljulia.sh
    Bug fix

--- init/dljulia.sh
+++ init/dljulia.sh
@@ -1,6 +1,6 @@
 mkdir ~/Apps
 cd ~/Apps
-wget -o julia.tar.gz https://julialangnightlies-s3.julialang.org/bin/linux/aarch64/julia-latest-linuxaarch64.tar.gz
+wget https://julialangnightlies-s3.julialang.org/bin/linux/aarch64/julia-latest-linuxaarch64.tar.gz
 mkdir julia
-tar -xvf julia.tar.gz -C /julia --strip-components 1
+tar -xvf julia-latest-linuxaarch64.tar.gz -C julia --strip-components 1
 sudo ln -s ~/Apps/julia/bin/julia /usr/bin/julia

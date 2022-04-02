e280b132c8013c7477809b091a7884fa7e3ac4bd
naturalog-data <naturalog.space>
2022-04-02 18:11:36 +0000

    cleaning files

---
+++ init/initcluster.sh
@@ -0,0 +1,5 @@
+mkdir ~/.termux/boot
+cd ~/.termux/boot
+wget https://raw.githubusercontent.com/Naturalog-data/QuarksyNet/main/startssh
+cd ~/
+wget https://raw.githubusercontent.com/Naturalog-data/QuarksyNet/main/DLNetHunter
